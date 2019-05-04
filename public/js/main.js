c87b46d73efd255dc96811350467c507b4bd5f92
nguyenthang1597 <nguyenthang1597@gmail.com>
2019-05-04 11:16:12 +0700

    Sd

--- public/js/main.js
+++ public/js/main.js
@@ -136,7 +136,7 @@ window.onbeforeunload = function () {
 /////////////////////////////////////////////////////////
 var pcConfig = {
   'iceServers': [{
-    'urls': 'turn:172.24.28.176:3010?transport=udp',
+    'urls': 'turn:10.30.80.62:3010?transport=udp',
     "username": "thaonk",
     "credential": "123456"
   }]
